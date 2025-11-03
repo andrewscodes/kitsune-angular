015fcd3d1e257e4cafdb0e42a99792655ef278ac
Andres Felipe <andrortiz97@hotmail.com>
2025-11-02 21:28:53 -0500

    feat: update build script and add announcement bar component
    - Removed base-href from build script in package.json
    - Added announcement bar component to app.component.html and app.component.ts
    - Adjusted styles in home.component.scss for better layout
    - Deleted unused kitsune.ico file
    - Updated images for kitsoone_logo.png, onyx.png, and added new social media icons (discord.svg, facebook.svg, github.svg, instagram.svg, youtube.svg)
    - Changed primary accent color in themes.scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
 import { HomeComponent } from './components/home/home.component';
+import { AnnouncementBarComponent } from './components/announcement-bar/announcement-bar.component';
 
 @Component({
   selector: 'app-main',
   imports: [
+    AnnouncementBarComponent,
     NavMenuComponent,
     HomeComponent
   ],
---
+++ src/app/components/announcement-bar/announcement-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-announcement-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './announcement-bar.component.html',
+  styleUrls: ['./announcement-bar.component.scss']
+})
+export class AnnouncementBarComponent {
+
+}
